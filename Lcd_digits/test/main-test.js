50fc3e29c2ae09eac021da56189b370fbcdf775e
mahong <1025383143@qq.com>
2016-04-28 18:07:55 +0800

    最新提交测试

--- Lcd_digits/test/main-test.js
+++ Lcd_digits/test/main-test.js
@@ -1,103 +1,60 @@
-/**
- * Created by sun on 16-4-21.
- */
-describe("lcd_digits",function(){
+describe("lcd_digits", function () {
     var lcds;
 
-    beforeEach(function(){
-        lcds=loadLcdDigits();
+    beforeEach(function () {
+        lcds = loadLcdDigits();
     });
 
-    it("should get the correct result",function(){
+    it("should get the correct result", function () {
 
         spyOn(console, 'log');
 
         creatLcdDigits("910");
 
-        expectText='._. ... ._. '+'\n'
-            +'|_| ..| |.| '+'\n'
-            +'..| ..| |_| ';
+        expectText = '._. ... ._. ' + '\n'
+            + '|_| ..| |.| ' + '\n'
+            + '..| ..| |_| ';
 
         expect(console.log).toHaveBeenCalledWith(expectText);
     });
 });
 
 
+describe("buildDigitArray()", function () {
 
+    it("should print correct array", function () {
 
-
-
-
-
-describe("buildDigitArray()",function(){
-
-   it("should print correct array",function(){
-
-       var digitArray=buildDigitArray("910");
-       expect(digitArray).toEqual(['9','1','0']);
-   })
+        var digitArray = buildDigitArray("910");
+        expect(digitArray).toEqual(['9', '1', '0']);
+    })
 });
 
-describe("buildLcdDigits()",function(){
+describe("buildLcdDigits()", function () {
 
-    it("should get the correct LCD_digits",function(){
+    it("should get the correct LCD_digits", function () {
 
-        var digitArray=['9','1','0'];
-        var lcds=loadLcdDigits();
-        
-        var lcdDigts=buildLcdDigts(digitArray,lcds);
+        var digitArray = ['9', '1', '0'];
+        var lcds = loadLcdDigits();
 
-        var expectResult=[{lcdDigit:{
-            firstline:'._.',
-            secondline:'|_|',
-            thirdline:'..|'
+        var lcdDigts = buildLcdDigts(digitArray, lcds);
 
-        }},{
-            lcdDigit:{
-                firstline:'...',
-                secondline:'..|',
-                thirdline:'..|'
-
-            }
-        },{lcdDigit:{
-            firstline:'._.',
-            secondline:'|.|',
-            thirdline:'|_|'
-
-        }}];
+        var expectResult = [['._.', '|_|', '..|'], ['...', '..|', '..|'], ['._.', '|.|', '|_|']];
 
         expect(lcdDigts).toEqual(expectResult);
     });
 });
 
-describe(" buildLcdDigitText()",function(){
-    
-    it("should print correct digits",function(){
+describe(" buildLcdDigitText()", function () {
 
-        var lcdDigits=[{lcdDigit:{
-            firstline:'._.',
-            secondline:'|_|',
-            thirdline:'..|'
+    it("should print correct digits", function () {
 
-        }},{
-            lcdDigit:{
-                firstline:'...',
-                secondline:'..|',
-                thirdline:'..|'
-
-            }
-        },{lcdDigit:{
-            firstline:'._.',
-            secondline:'|.|',
-            thirdline:'|_|'
-
-        }}];
+        var lcdDigits = [['._.', '|_|', '..|'], ['...', '..|', '..|'], ['._.', '|.|', '|_|']];
 
         spyOn(console, 'log');
         buildLcdDigitText(lcdDigits);
-        expectText='._. ... ._. '+'\n'
-                  +'|_| ..| |.| '+'\n'
-                  +'..| ..| |_| ';
+        expectText = '._. ... ._. ' + '\n'
+            + '|_| ..| |.| ' + '\n'
+            + '..| ..| |_| ';
         expect(console.log).toHaveBeenCalledWith(expectText);
 
     })
